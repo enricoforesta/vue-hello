d7e44bb214d172d57c05048ff45d3839b88751c0
enricoforesta <enrico.foresta97@gmail.com>
2023-11-27 15:28:17 +0100

    fix: ottimizzazione

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 "use strict"
-// Usiamo la destruttirazione per createAPP
-const { createApp } = Vue
+// Usiamo la destrutturazione per createAPP
+const { createApp } = Vue;
 // Variabile che contiene i data che ci servono.
   createApp({
     data() {
       return {
         title: "Ciao Classe",
         photo: "https://loremflickr.com/640/360",
+        alt: "Random Photo",
       }
     }
-  }).mount('#content')
+  }).mount('#content');
